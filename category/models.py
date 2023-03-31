0d7745d7d019a406b76eceeb09c075fca1671aba
Bishnu <thapabishnu20@gmail.com>
2023-03-31 20:08:31 +0545

    category model added & configured slug-field & installed pillow

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField()
+    desctiption = models.TextField(max_length=255)
+    cat_image = models.ImageField(upload_to='photos/categories', blank=True)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'

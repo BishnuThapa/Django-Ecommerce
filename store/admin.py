7e710a5b551178536899146edb40c1580c76943b
Bishnu <thapabishnu20@gmail.com>
2023-04-09 12:57:44 +0545

    variation model created

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Variation
 # Register your models here.
 
 
@@ -8,3 +8,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('product_name', 'price', 'stock', 'category',
                     'created_date', 'updated_date', 'is_available')
     prepopulated_fields = {"slug": ("product_name",)}
+
+
+@admin.register(Variation)
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product', 'variation_category',
+                    'variation_value', 'is_active', 'created_date',)
+    list_editable = ('is_active',)
+    list_filter = ('product', 'variation_category', 'variation_value')
